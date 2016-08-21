9e979a62aafdb0abc8cabaa8e92ee40852b9c1cf
Dave King <tildedave@gmail.com>
2016-08-21 09:23:11 -0700

    deserialization progress

--- src/rfc1035/core.clj
+++ src/rfc1035/core.clj
@@ -1,8 +1,9 @@
 (ns rfc1035.core
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.set :refer :all]))
 
 ; 4.
-(defrecord Message [header question answer authority additional])
+(defrecord Message [header questions answer-resources authority-resources additional-resources])
 
 ; 4.1.1
 (defrecord Header [id qr opcode aa tc rd ra z rcode qdcount ancount nscount])
@@ -16,6 +17,21 @@
 
 (defn buff [^String str]
   (vec (.getBytes str)))
+
+(defn byte-to-num
+  "Converts a byte buffer into a number"
+ [bytes]
+  (let [len (count bytes)]
+    (cond
+      (= len 1) (first bytes)
+      (= len 2) (+ (bit-shift-left (first bytes) 3) (second bytes)))))
+
+(defn bit-to-bool
+  "Converts a bit into true or false"
+  [byte]
+  (cond
+    (= byte 0) false
+    (= byte 1) true))
 
 (defn serialize-domain-name
   [domain-name]
@@ -40,43 +56,51 @@
   ; TODO https://tools.ietf.org/html/rfc5890 https://tools.ietf.org/html/rfc5891
   (take len (vec (.getBytes str "US-ASCII"))))
 
+; 3.2.4 CLASS values
+(def resource-class-map {:in 1 :cs 2 :ch 3 :hs 4})
+
 (defn serialize-resource-class [resource-class]
-  ; 3.2.4 CLASS values
-  (resource-class {:in 1 :cs 2 :ch 3 :hs 4}))
+  (resource-class resource-class-map))
 
-(defn serialize-resource-type [resource-type]
+(def resource-type-map {
   ; 3.2.2 TYPES values
-  (resource-type {
-    :a     1
-    :ns    2
-    :md    3
-    :mf    4
-    :cname 5
-    :soa   6
-    :mb    7
-    :mg    8
-    :mr    9
-    :null  10
-    :wks   11
-    :ptr   12
-    :hinfo 13
-    :minfo 14
-    :mx    15
-    :txt   16
-    :srv   33
-  }))
+  :a     1
+  :ns    2
+  :md    3
+  :mf    4
+  :cname 5
+  :soa   6
+  :mb    7
+  :mg    8
+  :mr    9
+  :null  10
+  :wks   11
+  :ptr   12
+  :hinfo 13
+  :minfo 14
+  :mx    15
+  :txt   16
+  :srv   33
+})
 
-(defn serialize-query-type [query-type]
+(def response-code-map {
+  :no-error        0
+  :format-error    1
+  :server-failure  2
+  :name-error      3
+  :not-implemented 4
+  :refused         5
+})
+
+(def query-type-map (merge resource-type-map {
   ; 3.2.3 QTYPE values
-  (let [resource-type (serialize-resource-type query-type)]
-    (if (some? resource-type) resource-type (query-type {
-    :axfr 252
-    :mailb 253
-    :maila 254
-    :* 255
-}))))
+  :axfr 252
+  :mailb 253
+  :maila 254
+  :* 255
+}))
 
-(defn serialize-opcode [opcode] (opcode {:query 0 :iquery 1 :status 2}))
+(def opcode-map {:query 0 :iquery 1 :status 2})
 
 (defn serialize-question
   "Serialize a question"
@@ -85,32 +109,8 @@
     (concat
       (list (count domain-name-buff))
       domain-name-buff
-      (pack-num (serialize-resource-type (:qtype question)) 2)
-      (pack-num (serialize-resource-class (:qclass question)) 2))))
-
-(defn serialize-header
-  "Serialize a header"
-  [header]
-  (concat
-    (pack-num (:id header) 2)
-    (pack-num
-      (bit-or
-        (if (:qr header) 8 0)
-        (serialize-opcode (:opcode header)))
-      1)
-    (pack-num
-      (bit-or
-        (if (:aa header) 8 0)
-        (if (:tc header) 4 0)
-        (if (:rd header) 2 0)
-        (if (:ra header) 1 0))
-      1)
-    (pack-num 0 1) ; "Z" part of header reserved for future use
-    (pack-num (:rcode header) 1)
-    (pack-num (:qdcount header) 2)
-    (pack-num (:ancount header) 2)
-    (pack-num (:nscount header) 2)
-    (pack-num (:arcount header) 2)))
+      (pack-num ((:qtype question) resource-type-map) 2)
+      (pack-num ((:qclass question) resource-class-map) 2))))
 
 (defrecord Header [id qr opcode aa tc rd ra rcode qdcount ancount nscount arcount])
 
@@ -122,13 +122,74 @@
   "Serialize a resource record"
   [resource-record])
 
-(defn make-query [id domain-name resource-type resource-class]
+(defn serialize-header
+  "Serialize a header"
+  [header]
+  (vec (concat
+    (pack-num (:id header) 2)
+    (pack-num
+      (bit-or
+        (bit-shift-left (if (:qr header) 1 0) 6)
+        (bit-shift-left ((:opcode header) opcode-map) 4)
+        (if (:aa header) 8 0)
+        (if (:tc header) 4 0)
+        (if (:rd header) 2 0)
+        (if (:ra header) 1 0))
+      1)
+    (pack-num
+      (bit-and
+        ; "Z" part of header reserved for future use
+        15
+        (:rcode header))
+      1)
+    (pack-num (:qdcount header) 2)
+    (pack-num (:ancount header) 2)
+    (pack-num (:nscount header) 2)
+    (pack-num (:arcount header) 2))))
+
+(defn make-query-message [id domain-name resource-type resource-class]
   ; 1 question
   (let [header (->Header id 0 :query false false false false 0 1 0 0 0)
         question (->Question domain-name resource-type resource-class)]
-    (concat
-      (serialize-header header)
-      (serialize-question question))))
+    (->Message header [question] [] [] [])))
+
+(defn serialize-message
+  [message]
+  (vec (flatten
+    (list
+      (serialize-header (:header message))
+      (map serialize-question (:questions message))
+      (map serialize-resource-record (:answer-resources message))
+      (map serialize-resource-record (:authority-resources message))
+      (map serialize-resource-record (:additional-resources message))))))
+
+(defn deserialize-header
+  "Deserialize bytes into a message header"
+  [header-bytes]
+  (let [id      (byte-to-num (subvec header-bytes 0 2))
+        qr      (bit-test (get header-bytes 2) 7)
+        opcode  (get (map-invert opcode-map)
+                  (bit-shift-right
+                    (bit-and 7 (get header-bytes 2))
+                    4))
+        aa      (bit-test (bit-shift-right (get header-bytes 2) 4) 3)
+        tc      (bit-test (get header-bytes 2) 2)
+        rd      (bit-test (get header-bytes 2) 1)
+        ra      (bit-test (get header-bytes 2) 0)
+        ; upper 4 bits of 4th byte = Z code and is ignored
+        rcode   (get (map-invert response-code-map) (bit-and 15 (get header-bytes 3)))
+        qdcount (byte-to-num (subvec header-bytes 4 6))
+        ancount (byte-to-num (subvec header-bytes 6 8))
+        nscount (byte-to-num (subvec header-bytes 8 10))
+        arcount (byte-to-num (subvec header-bytes 10 12))]
+    (->Header id qr opcode aa tc rd ra rcode qdcount ancount nscount arcount)))
+
+(defn deserialize-message
+  "Deserialize a message into its component parts"
+  [message-bytes]
+  ; header is 12 bytes
+  (let [header (deserialize-header (subvec message-bytes 0 12))]
+    (->Message header [] [] [] [])))
 
 ; 4.1.4 Message Compression TODO
 
--- test/rfc1035/core_test.clj
+++ test/rfc1035/core_test.clj
@@ -3,17 +3,17 @@
             [rfc1035.core :refer :all]))
 
 (deftest test-serialize
-  (is (= (serialize-resource-type :a)     1))
-  (is (= (serialize-resource-type :ns)    2))
-  (is (= (serialize-resource-type :cname) 5))
-  (is (= (serialize-resource-type :ptr)   12))
-  (is (= (serialize-resource-type :mx)    15))
-  (is (= (serialize-resource-type :txt)   16))
-  (is (= (serialize-resource-type :srv)   33))
-  (is (= (serialize-query-type :a)        1))
-  (is (= (serialize-query-type :*)        255))
-  (is (= (serialize-resource-class :in)      1))
-  (is (= (serialize-resource-class :ch)      3))
+  (is (= (resource-type-map :a)     1))
+  (is (= (resource-type-map :ns)    2))
+  (is (= (resource-type-map :cname) 5))
+  (is (= (resource-type-map :ptr)   12))
+  (is (= (resource-type-map :mx)    15))
+  (is (= (resource-type-map :txt)   16))
+  (is (= (resource-type-map :srv)   33))
+  (is (= (query-type-map :a)        1))
+  (is (= (query-type-map :*)        255))
+  (is (= (resource-class-map :in)   1))
+  (is (= (resource-class-map :ch)   3))
   ; unclear if questions are correct
   (is (=
     (serialize-domain-name "F.ISI.ARPA")
@@ -28,5 +28,10 @@
     (serialize-question (->Question "www.google.com" :mx :ch))
     '(16 3 87 87 87 6 71 79 79 71 76 69 3 67 79 77 0 0 15 0 3)))
   (is (=
+    (count (serialize-header (->Header 123 0 :query false false false false 0 1 0 0 0)))
+    12)
+    "Header serialized to expected length")
+  (is (=
     (serialize-header (->Header 123 0 :query false false false false 0 1 0 0 0))
-    '(0 123 8 0 0 0 0 1 0 0 0 0 0 0))))
+    '(0 123 64 0 0 1 0 0 0 0 0 0))
+    "Header is as expected"))
