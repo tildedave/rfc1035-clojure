429b983153cabcd5ca8067a41ab2607b984babb1
Dave King <tildedave@gmail.com>
2016-08-20 21:54:00 -0700

    further progress

--- src/rfc1035/core.clj
+++ src/rfc1035/core.clj
@@ -1,4 +1,5 @@
-(ns rfc1035.core)
+(ns rfc1035.core
+  (:require [clojure.string :as str]))
 
 ; 4.
 (defrecord Message [header question answer authority additional])
@@ -13,18 +14,28 @@
 ; rlength will be added during serialization
 (defrecord ResourceRecord [name type class ttl rdata])
 
-(defn buff [str]
+(defn buff [^String str]
   (vec (.getBytes str)))
+
+(defn serialize-domain-name
+  [domain-name]
+  "Serialize a domain name"
+  (concat
+    (reduce
+      #(concat %1 (list (count %2)) (buff %2))
+      (vector)
+      (str/split (str/upper-case domain-name) #"\."))
+    (list 0)))
 
 (defn pack-num [n len]
   "Converts a number into a byte array with a given padding"
   (let [buff (java.nio.ByteBuffer/allocate len)]
-    (vec (.array
+    (vec
       (cond
-        (= len 1) (byte n)
-        (= len 2) (.putShort buff n))))))
+        (= len 1) [(byte n)]
+        (= len 2) (.array (.putShort buff n))))))
 
-(defn pack-str [str len]
+(defn pack-str [^String str len]
   "Converts a string into a byte array with a given padding"
   ; TODO https://tools.ietf.org/html/rfc5890 https://tools.ietf.org/html/rfc5891
   (take len (vec (.getBytes str "US-ASCII"))))
@@ -65,19 +76,59 @@
     :* 255
 }))))
 
+(defn serialize-opcode [opcode] (opcode {:query 0 :iquery 1 :status 2}))
+
 (defn serialize-question
   "Serialize a question"
   [question]
-  (let [domain-name-buff (buff (:qname question))]
+  (let [domain-name-buff (serialize-domain-name (:qname question))]
     (concat
       (list (count domain-name-buff))
       domain-name-buff
       (pack-num (serialize-resource-type (:qtype question)) 2)
       (pack-num (serialize-resource-class (:qclass question)) 2))))
 
+(defn serialize-header
+  "Serialize a header"
+  [header]
+  (concat
+    (pack-num (:id header) 2)
+    (pack-num
+      (bit-or
+        (if (:qr header) 8 0)
+        (serialize-opcode (:opcode header)))
+      1)
+    (pack-num
+      (bit-or
+        (if (:aa header) 8 0)
+        (if (:tc header) 4 0)
+        (if (:rd header) 2 0)
+        (if (:ra header) 1 0))
+      1)
+    (pack-num 0 1) ; "Z" part of header reserved for future use
+    (pack-num (:rcode header) 1)
+    (pack-num (:qdcount header) 2)
+    (pack-num (:ancount header) 2)
+    (pack-num (:nscount header) 2)
+    (pack-num (:arcount header) 2)))
+
+(defrecord Header [id qr opcode aa tc rd ra rcode qdcount ancount nscount arcount])
+
+; (defn serialize-remote-query
+;   "Serialize a remote query"
+;   [id question]
+
 (defn serialize-resource-record
   "Serialize a resource record"
   [resource-record])
+
+(defn make-query [id domain-name resource-type resource-class]
+  ; 1 question
+  (let [header (->Header id 0 :query false false false false 0 1 0 0 0)
+        question (->Question domain-name resource-type resource-class)]
+    (concat
+      (serialize-header header)
+      (serialize-question question))))
 
 ; 4.1.4 Message Compression TODO
 
--- test/rfc1035/core_test.clj
+++ test/rfc1035/core_test.clj
@@ -15,8 +15,18 @@
   (is (= (serialize-resource-class :in)      1))
   (is (= (serialize-resource-class :ch)      3))
   ; unclear if questions are correct
-  (is (= (serialize-question (->Question "www.google.com" :a :in))
-    '(14 119 119 119 46 103 111 111 103 108 101 46 99 111 109 0 1 0 1)))
-  (is (= (serialize-question (->Question "www.google.com" :mx :ch))
-    '(14 119 119 119 46 103 111 111 103 108 101 46 99 111 109 0 15 0 3)))
-  )
+  (is (=
+    (serialize-domain-name "F.ISI.ARPA")
+    '(1 70 3 73 83 73 4 65 82 80 65 0)))
+  (is (=
+    (serialize-domain-name "F.ISI.ARPA")
+    (serialize-domain-name "f.isi.arpa")))
+  (is (=
+    (serialize-question (->Question "www.google.com" :a :in))
+    '(16 3 87 87 87 6 71 79 79 71 76 69 3 67 79 77 0 0 1 0 1)))
+  (is (=
+    (serialize-question (->Question "www.google.com" :mx :ch))
+    '(16 3 87 87 87 6 71 79 79 71 76 69 3 67 79 77 0 0 15 0 3)))
+  (is (=
+    (serialize-header (->Header 123 0 :query false false false false 0 1 0 0 0))
+    '(0 123 8 0 0 0 0 1 0 0 0 0 0 0))))
